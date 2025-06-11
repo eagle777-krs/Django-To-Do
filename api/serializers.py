ee7d20e4676e221e7a23743f4cea56d2254874fc
eagle777-krs <williamgarfield67@gmail.com>
2025-06-11 10:48:56 +0300

    fix serializers

--- api/serializers.py
+++ api/serializers.py
@@ -2,15 +2,13 @@ from rest_framework import serializers
 from tasks.models import Task, Category
 
 class TaskSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=200)
-    created_at = serializers.DateField()
-    deadline = serializers.DateField()
-    category = serializers.CharField(source='category.name', max_length=100)
-    username = serializers.CharField(source='user.username', max_length=100)
+    class Meta:
+        model = Task
+        exclude = ('category', 'user', 'status')
 
 class UPDTaskSerializer(serializers.Serializer):
     category = serializers.ReadOnlyField(source='Category.name')
     c =serializers.ReadOnlyField(source='Category.priority')
     class Meta:
         model = Task
-        fields = ['id', 'name', 'description', 'created_at', 'deadline', 'category', 'category', 'status']
+        fields = ('id', 'name', 'description', 'created_at', 'deadline', 'category', 'category', 'status')
